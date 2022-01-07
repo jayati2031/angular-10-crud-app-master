289e6c84c47d51f3d647d3ca6c7d0a31611a26c4
Jayati2031 <61575158+Jayati2031@users.noreply.github.com>
2022-01-07 19:37:27 +0530

    first commit

---
+++ src/app/components/registration/registration.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+  admin = {
+    emailid: '',
+    password:''
+    
+  };
+  submitted = false;
+
+  constructor(private adminService: AdminService ) { }
+
+  ngOnInit(): void {
+  }
+
+  saveAdmin(): void {
+    if(this.admin.emailid=="jayati@gmail.com" && this.admin.password=="password"){
+      const data = {
+        emailid: this.admin.emailid,
+        password: this.admin.password,
+      };
+  
+      this.adminService.create(data)
+        .subscribe(
+          response => {
+            console.log(response);
+            this.submitted = true;
+          },
+          error => {
+            console.log(error);
+          });
+    }
+    else{
+      alert("Invalid credentials");
+    }
+    }
+    
+
+  newAdmin(): void {
+    this.submitted = false;
+    this.admin = {
+      emailid: '',
+      password: '',
+      
+    };
+  }
+
+}
